3b630fb071f83aa91c48e484e42118b5d3c5e24e
Greg Loomis <gregory.loomis@fcps.org>
2025-09-25 14:00:33 -0400

    story 1
    Completed with mr. loomis

--- MadLibs/src/madlibs/Madlibs.java
+++ MadLibs/src/madlibs/Madlibs.java
@@ -1,9 +1,38 @@
 package madlibs;
 
+import java.util.Scanner;
+
 public class Madlibs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a name");
+		String name = scan.nextLine();
+
+		System.out.println("Enter a animal");
+		String animal = scan.nextLine();
+
+		System.out.println("Enter a noun");
+		String noun = scan.nextLine();
+
+		System.out.println("Enter a adjective");
+		String adj = scan.nextLine();
+
+		System.out.println("Enter a noun");
+		String noun2 = scan.nextLine();
+
+		System.out.println("Enter a verb");
+		String verb = scan.nextLine();
+
+		//NAME had a little ANIMAL, whose NOUN was ADJ as NOUN2. 
+		//Everywhere that NAME went, the ANIMAL was sure to VERB
+
+		System.out.print(name + " had a little " + animal);
+		System.out.println(", whose " + noun + " was " + adj + " as " + noun2);
+
+		System.out.print("Everywhere that "+name+" went,");
+		System.out.println(" the " + animal + " was sure to " + verb);
 
 	}
 
